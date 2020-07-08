4796321f0390c93f5867775b75d645f6eae9324c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-08 22:48:08 +0100

    Set key for each list item in MovieList

--- components/MovieList/MovieList.jsx
+++ components/MovieList/MovieList.jsx
@@ -2,6 +2,21 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
 import {baseUrl, token} from "../../env";
 
+const MOVIES = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
 
@@ -20,9 +35,10 @@ const MovieList = () => {
   return (
     <View style={styles.container}>
       <FlatList
+        keyExtractor={item => item.uuid}
         data={movies}
         renderItem={({item}) => (
-          <Text key={item.id}>{item.title}</Text>
+          <Text>{item.title}</Text>
         )}
       />
     </View>
