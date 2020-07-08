d9f698ec389a8014149fc9771ef7902be0c0c9a6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-08 23:15:51 +0100

    Set loading state while fetching API data

--- components/MovieList/MovieList.jsx
+++ components/MovieList/MovieList.jsx
@@ -2,23 +2,9 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, FlatList} from 'react-native';
 import {baseUrl, token} from "../../env";
 
-const MOVIES = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${baseUrl}/api/movies/`, {
@@ -29,18 +15,26 @@ const MovieList = () => {
     })
       .then(res => res.json())
       .then(movies => setMovies(movies))
+      .then(() => setLoading(false))
       .catch(error => console.log(error))
   }, [])
 
   return (
     <View style={styles.container}>
-      <FlatList
-        keyExtractor={item => item.uuid}
-        data={movies}
-        renderItem={({item}) => (
-          <Text>{item.title}</Text>
-        )}
-      />
+      {
+        loading &&
+        <Text>Loading...</Text>
+      }
+      {
+        !loading &&
+        <FlatList
+          keyExtractor={item => item.uuid}
+          data={movies}
+          renderItem={({item}) => (
+            <Text>{item.title}</Text>
+          )}
+        />
+      }
     </View>
   );
 }
