32870f535ed174b8b7ce047b44af6d56894072e7
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-09 10:53:35 +0100

    Added logo in App and applied custom styles

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import MovieList from "./components/MovieList/MovieList";
+import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
+import MovieRaterLogo from './assets/movie-rater-logo.png';
 
 const App = () => {
   return (
-    <MovieList />
+    <View>
+      <Image
+        source={MovieRaterLogo}
+        style={styles.logo}
+        reziseMode='contain'
+      />
+      <MovieList/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: '100%',
+    marginTop: 30,
+  },
+});
 
 export default App;
