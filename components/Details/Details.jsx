7b3b01de697cdaad5e250500769ea7c9ec8fc61c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-10 23:59:09 +0100

    Added rating stars and applied conditional styling to show rating value in Details

--- components/Details/Details.jsx
+++ components/Details/Details.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faStar} from "@fortawesome/free-solid-svg-icons/faStar";
 
 const Details = (props) => {
   // We get the relevant prop (1st argument) and specify a default (2nd argument)
@@ -7,7 +9,16 @@ const Details = (props) => {
 
   return (
     <View>
-      <Text>Details about {movie.title}</Text>
+      <Text>{movie.title}</Text>
+      <View style={styles.ratingContainer}>
+        <FontAwesomeIcon style={movie.avg_rating > 0 ? styles.orange : styles.grey} icon={faStar}/>
+        <FontAwesomeIcon style={movie.avg_rating > 1 ? styles.orange : styles.grey} icon={faStar}/>
+        <FontAwesomeIcon style={movie.avg_rating > 2 ? styles.orange : styles.grey} icon={faStar}/>
+        <FontAwesomeIcon style={movie.avg_rating > 3 ? styles.orange : styles.grey} icon={faStar}/>
+        <FontAwesomeIcon style={movie.avg_rating > 4 ? styles.orange : styles.grey} icon={faStar}/>
+        <Text style={styles.numRatings}>({movie.no_of_ratings})</Text>
+      </View>
+      <Text>{movie.description}</Text>
     </View>
   );
 }
@@ -32,6 +43,21 @@ const styles = StyleSheet.create({
   itemText: {
     color: '#fff',
     fontSize: 24,
+  },
+  ratingContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  orange: {
+    color: 'orange',
+  },
+  grey: {
+    color: '#999'
+  },
+  numRatings: {
+    marginLeft: 5,
   },
 });
 
