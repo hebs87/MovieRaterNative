5ff6d237378f0309452822154dcb7e5850a2dc28
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-10 23:08:31 +0100

    Created basic details component and set up new nav link for it

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MovieList from "./components/MovieList/MovieList";
+import Details from "./components/Details/Details";
 import {createAppContainer} from "react-navigation";
 import {createStackNavigator} from "react-navigation-stack";
 
 const AppNavigator = createStackNavigator({
   MovieList: {screen: MovieList},
+  Details: {screen: Details},
 })
 
 const App = createAppContainer(AppNavigator);
---
+++ components/Details/Details.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Details = () => {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: '100%',
+    marginTop: 30,
+  },
+  item: {
+    flex: 1,
+    padding: 10,
+    height: 50,
+    backgroundColor: '#282C35',
+  },
+  itemText: {
+    color: '#fff',
+    fontSize: 24,
+  },
+});
+
+export default Details;
