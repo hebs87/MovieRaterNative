23562d3edb82ba861b2c2591cf94815397e010a8
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-13 11:29:11 +0100

    Added form fields and save button to Edit form

--- components/Edit/Edit.js
+++ components/Edit/Edit.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const Edit = (props) => {
   // We get the relevant prop (1st argument) and specify a default (2nd argument)
   const movie = props.navigation.getParam('movie', null);
+  const [title, setTitle] = useState(movie.title);
+  const [description, setDescription] = useState(movie.description);
+
+  const saveMovie = () => {
+    props.navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.description}>Edit {movie.title}</Text>
+      <Text style={styles.label}>Title:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Title'
+        onChangeText={text => setTitle(text)}
+        value={title}
+      />
+      <Text style={styles.label}>Description:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Description'
+        onChangeText={text => setDescription(text)}
+        value={description}
+      />
+      <Button
+        title='Save'
+        color='orange'
+        onPress={() => saveMovie()}
+      />
     </View>
   );
 }
@@ -31,10 +55,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#282C35',
     padding: 10,
   },
-  description: {
+  label: {
     fontSize: 20,
     padding: 10,
     color: 'white',
+  },
+  input: {
+    fontSize: 24,
+    backgroundColor: '#fff',
+    padding: 10,
+    margin: 10,
   },
 });
 
